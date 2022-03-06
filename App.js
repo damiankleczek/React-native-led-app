55e81424c55966baab67c924c7073a9c187bd5d7
Damian Kłeczek <damian.kleczek97@gmail.com>
2022-03-06 23:21:58 +0100

    implemented mapping function to dysplay elements as individual components

--- App.js
+++ App.js
@@ -1,15 +1,105 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native';
+import {StyleSheet, View, SafeAreaView, StatusBar} from 'react-native';
 import Element from './shared/Element';
 
 const App = () => {
   console.warn = () => {};
+
+  const [leds, setLeds] = useState([
+    {
+      key: 1,
+      label: 'LED1',
+      isSwitchOn: false,
+      isModalVisible: false,
+      color: 'yellow',
+      sliderValue: 10,
+    },
+    {
+      key: 2,
+      label: 'LED2',
+      isSwitchOn: false,
+      isModalVisible: false,
+      color: 'orange',
+      sliderValue: 30,
+    },
+    {
+      key: 3,
+      label: 'LED3',
+      isSwitchOn: false,
+      isModalVisible: false,
+      color: 'red',
+      sliderValue: 50,
+    },
+  ]);
+
+  const updateSwitch = (leds, i) => {
+    let newLeds = [...leds];
+    newLeds[i] = {
+      ...newLeds[i],
+      isSwitchOn: !newLeds[i].isSwitchOn,
+    };
+    setLeds(newLeds);
+  };
+
+  const updateModal = (leds, i, isVisible) => {
+    let newLeds = [...leds];
+    newLeds[i] = {
+      ...newLeds[i],
+      isModalVisible: isVisible,
+    };
+    setLeds(newLeds);
+  };
+
+  const updateModalOnOk = (leds, i, isVisible, color) => {
+    let newLeds = [...leds];
+    newLeds[i] = {
+      ...newLeds[i],
+      isModalVisible: isVisible,
+      color: color,
+    };
+    setLeds(newLeds);
+  };
+
+  const updateSlider = (leds, i, value) => {
+    let newLeds = [...leds];
+    newLeds[i] = {
+      ...newLeds[i],
+      sliderValue: value,
+    };
+    setLeds(newLeds);
+  };
+
+  // const updateSwitch = (leds, i, led, key) => {
+  //   setLeds((prevLeds) => {
+  //     return [
+  //       ...prevLeds,
+  //       {...prevLeds[i], isSwitchOn: !prevLeds[i].isSwitchOn},
+  //     ];
+  //     // return led.key === key ? {...led, isSwitchOn: !led.isSwitchOn} : led;
+  //   });
+  // };
+
+  let list = () => {
+    return leds.map((led, i) => {
+      return (
+        <Element
+          key={led.key}
+          led={led}
+          leds={leds}
+          i={i}
+          updateSwitch={updateSwitch}
+          updateModalOnOk={updateModalOnOk}
+          updateModal={updateModal}
+          updateSlider={updateSlider}
+        />
+      );
+    });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={styles.statusbar.backgroundColor}></StatusBar>
-      <View style={styles.content}>
-        <Element />
-      </View>
+      <View style={styles.content}>{list()}</View>
     </SafeAreaView>
   );
 };
--- shared/Element.js
+++ shared/Element.js
@@ -1,20 +1,19 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native';
-import tinycolor from 'tinycolor2';
+import {StyleSheet, Text, View} from 'react-native';
 import CustomSwitch from './CustomSwitch';
 import LightnessSlider from './LightnessSlider';
 import ColorPicker from './ColorPicker';
+import tinycolor from 'tinycolor2';
 
-const Element = () => {
-  const [leds, setLeds] = useState({
-    key: 1,
-    label: 'LED1',
-    isSwitchOn: false,
-    isModalVisible: false,
-    color: 'yellow',
-    sliderValue: 10,
-  });
-
+const Element = ({
+  led,
+  leds,
+  i,
+  updateSwitch,
+  updateModal,
+  updateModalOnOk,
+  updateSlider,
+}) => {
   const [recentsThree, setRecentsThree] = useState([
     '#247ba0',
     '#70c1b3',
@@ -26,13 +25,13 @@ const Element = () => {
   return (
     <View style={styles.elementHolder}>
       <View style={styles.element}>
-        <Text style={styles.text}>{leds.label}</Text>
+        <Text style={styles.text}>{led.label}</Text>
         <CustomSwitch
-          onValueChange={(toggleValue) => {
-            leds.isSwitchOn = toggleValue;
-            setLeds({...leds, toggleValue});
+          onValueChange={() => {
+            updateSwitch(leds, i);
           }}
-          value={leds.isSwitchOn}
+          // onValueChange={updateSwitch(leds, i, led, led.key)}
+          value={led.isSwitchOn}
         />
       </View>
 
@@ -41,26 +40,16 @@ const Element = () => {
       <View style={styles.colorArea}>
         <Text style={styles.colorPickerLabel}>Kolor:</Text>
         <ColorPicker
-          onPress={() =>
-            setLeds({
-              ...leds,
-              isModalVisible: true,
-            })
-          }
-          visible={leds.isModalVisible}
-          color={leds.color}
-          onCancel={() =>
-            setLeds({
-              ...leds,
-              isModalVisible: false,
-            })
-          }
+          onPress={() => {
+            updateModal(leds, i, true);
+          }}
+          visible={led.isModalVisible}
+          color={led.color}
+          onCancel={() => {
+            updateModal(leds, i, false);
+          }}
           onOk={(colorHex) => {
-            setLeds({
-              ...leds,
-              isModalVisible: false,
-              color: tinycolor(colorHex).toHsl(),
-            }),
+            updateModalOnOk(leds, i, false, tinycolor(colorHex).toHsl()),
               setRecentsThree([
                 colorHex,
                 ...recentsThree.filter((c) => c !== colorHex).slice(0, 4),
@@ -72,11 +61,13 @@ const Element = () => {
 
       <View style={styles.lightnessArea}>
         <Text style={styles.lightnessAreaText}>
-          Jasność: {leds.sliderValue}%
+          Jasność: {led.sliderValue}%
         </Text>
         <LightnessSlider
-          value={leds.sliderValue}
-          onValueChange={(value) => setLeds({...leds, sliderValue: value})}
+          value={led.sliderValue}
+          onValueChange={(value) => {
+            updateSlider(leds, i, value);
+          }}
         />
       </View>
     </View>
